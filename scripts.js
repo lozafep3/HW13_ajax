85b8cafafb45722e369bff5df119ca6cc2c8573b
Arthur Loza <loza.uns@gmail.com>
2017-07-28 01:43:03 +0300

    HW13_ajax: fixed

--- scripts.js
+++ scripts.js
@@ -5,37 +5,45 @@ var draw = document.forms.someform.elements.draw;
 var posts = document.getElementById('posts');
 var xhr = new XMLHttpRequest();
 
-// Пост добавляется в bd.json, но xhr.status ВСЕГДА возвращает 0, как бы я не пробовал - проверку не сделать.
-// xhr.onreadystatechange соответственно никогда не выполняется. Без понятия, в чем проблема.
-
 submit.onclick = function () {
     xhr.open('POST', 'http://localhost:3000/posts');
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 201) {
+                console.log(JSON.parse(xhr.responseText));
+            } else {
+                console.log( xhr.status + ': ' + xhr.statusText );
+            }
+        }
+    };
+
     xhr.send(JSON.stringify({title: title.value, author: author.value}));
 };
 
-// Получение постов работает, но странно. Если не отправлять новый GET запрос, выводится последний отправленный вариант.
-// xhr.responseText возвращает строку:
-//{
-//"title": "Test1",
-//"author": "Test2",
-//"id": 27
-//}
-//Но в JSON она не парсится почему-то.
-
-//Если писать новый GET запрос выбивает ошибку:
-// "SyntaxError: JSON.parse: unexpected end of data at line 1 column 1 of the JSON data"
-// xhr.responseText возвращает пустую строку.
-//Что я делаю не так?
-
 draw.onclick = function () {
-    //xhr.open('GET', 'http://localhost:3000/posts');
-    var data = xhr.responseText;
-    addData(data);
+    xhr.open('GET', 'http://localhost:3000/posts');
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText);
+                addData(data);
+            } else {
+                console.log( xhr.status + ': ' + xhr.statusText );
+            }
+        }
+    };
+
+    xhr.send();
 };
 
 function addData(data) {
-    var newData = document.createElement("p");
-    newData.innerText = data;
-    posts.appendChild(newData);
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+        var newData = document.createElement("p");
+        newData.innerText = data[i]['id'] + '. ' + data[i]['title'] + ' - ' + data[i]['author'];
+        posts.appendChild(newData);
+    }
 }
